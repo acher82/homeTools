84dc0a7ca422203202326a994d3be5a9ecd6d801
acher82 <chernomordik@gmail.com>
2018-04-01 22:12:34 +0300

    Add month to dataEntity and to command line args

--- BudgetTools/de.py
+++ BudgetTools/de.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 class dataEntity(object):
-    def __init__(self, date, place, summ, source):
+    def __init__(self, date, month, place, summ, source):
         self.date = date
+        self.month = month
         self.place = place
         self.summ = summ
         self.source = source
     def __str__(self):
-        return "{0},'{1}',{2},{3}".format(self.date, self.place.encode('utf-8'), self.summ, self.source)
+        return "{0},{1},'{2}',{3},{4}".format(self.date, self.month, self.place.encode('utf-8'), self.summ, self.source)
--- BudgetTools/parseExcelReport.py
+++ BudgetTools/parseExcelReport.py
@@ -7,16 +7,23 @@ from xlrd import open_workbook
 
 import de
 
-def is_valid_file(parser, arg):
-    if not os.path.exists(arg):
-        parser.error("The file %s does not exist!" % arg)
+def is_valid_file(p):
+    if not os.path.exists(p):
+        raise argparse.ArgumentTypeError("The file %s does not exist!" % p)
     else:
-        return arg
+        return p
+
+def is_valid_month(m):
+    m = int(m)
+    if m < 1 or m > 12:
+        raise argparse.ArgumentTypeError("Month must to be in rage 1..12")
+    return m
 
 parser = argparse.ArgumentParser(description="Consumption excel parser")
 parser.add_argument("-p","--path", dest="filename", required=True,
-                    help="path to excel file", metavar="FILE",
-                    type=lambda x: is_valid_file(parser, x))
+                    help="path to excel file", metavar="FILE", type=is_valid_file)
+parser.add_argument("-m", "--month", dest="month", required=True,
+                    help="report's month", type=is_valid_month)
 parser.add_argument("-s", "--source", dest="source", required=True,
                     help="excel's file source (last 4 digits of credid card or bank account number)")
 args = parser.parse_args()
@@ -34,6 +41,6 @@ for sheet in wb.sheets():
             if (value.startswith('מסטרקארד'.decode('utf-8'))):
                 source = ''.join(c for c in value if c.isdigit())
             elif source and re.match(r"\d{2}\/\d{2}\/\d{4}", value):
-                entities.append(de.dataEntity(value, sheet.cell(row,1).value, sheet.cell(row,4).value, source))
+                entities.append(de.dataEntity(value, args.month, sheet.cell(row,1).value, sheet.cell(row,4).value, source))
 for de in entities:
     print(de)
