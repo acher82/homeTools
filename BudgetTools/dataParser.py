196b50d87dee661dc5aba106f9fd41a109853d8c
acher82 <chernomordik@gmail.com>
2018-05-07 23:04:42 +0300

    Add Inernational deals parser to isracard report

--- BudgetTools/dataParser.py
+++ BudgetTools/dataParser.py
@@ -16,8 +16,14 @@ def parse_IsraNew(args):
                 value = sheet.cell(row,0).value
                 if (value.startswith('מסטרקארד'.decode('utf-8'))):
                     source = ''.join(c for c in value if c.isdigit())
+                    local = True
                 elif source and re.match(r"\d{2}\/\d{2}\/\d{4}", value):
-                    entities.append(de.dataEntity(value, args.month, sheet.cell(row,1).value, sheet.cell(row,4).value, source))
+                    if local:
+                        entities.append(de.dataEntity(value, args.month, sheet.cell(row,1).value, sheet.cell(row,4).value, source))
+                    elif not sheet.cell(row,2).value.startswith('TOTAL FOR DATE'):
+                        entities.append(de.dataEntity(value, args.month, sheet.cell(row,2).value, sheet.cell(row,5).value, source))
+                elif source and value.startswith('עסקאות בחו˝ל'.decode('utf-8')):
+                    local = False
 
     return entities
 
