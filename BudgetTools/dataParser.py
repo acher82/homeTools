37b31be92f8bf4d02f9fd080b22f4516ee9d9068
acher82 <chernomordik@gmail.com>
2019-02-03 20:14:22 +0200

    Fix bug in IsraNew parser

--- BudgetTools/dataParser.py
+++ BudgetTools/dataParser.py
@@ -17,7 +17,7 @@ def parse_IsraNew(args):
         for row in range(sheet.nrows):
             if sheet.cell(row,0).value :
                 value = sheet.cell(row,0).value
-                if (value.startswith('מסטרקארד'.decode('utf-8'))):
+                if 'דראקרטסמ'.decode('utf-8') in value or 'מסטרקארד'.decode('utf-8') in value:
                     source = ''.join(c for c in value if c.isdigit())
                     local = True
                 elif source and re.match(r"\d{2}\/\d{2}\/\d{4}", value):
@@ -25,7 +25,7 @@ def parse_IsraNew(args):
                         entities.append(de.dataEntity(value, args.month, sheet.cell(row,1).value, sheet.cell(row,4).value, source))
                     elif not sheet.cell(row,2).value.startswith('TOTAL FOR DATE'):
                         entities.append(de.dataEntity(value, args.month, sheet.cell(row,2).value, sheet.cell(row,5).value, source))
-                elif source and value.startswith('עסקאות בחו˝ל'.decode('utf-8')):
+                elif source and 'עסקאות בחו˝ל'.decode('utf-8') in value:
                     local = False
 
     return entities
