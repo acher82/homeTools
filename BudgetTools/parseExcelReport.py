50503ae8d69662ff177cdd56e14104a53aeea0b9
acher82 <chernomordik@gmail.com>
2019-02-15 13:59:29 +0200

    Change month format

--- BudgetTools/parseExcelReport.py
+++ BudgetTools/parseExcelReport.py
@@ -1,35 +1,35 @@
 # -*- coding: utf-8 -*-
 import argparse
 import os.path
-import sys
+import re
 
 import dataParser
 import de
 
 
-def is_valid_file(p):
-    if not os.path.exists(p):
-        raise argparse.ArgumentTypeError("The file %s does not exist!" % p)
+def is_valid_file(path):
+    if not os.path.exists(path):
+        raise argparse.ArgumentTypeError("The file %s does not exist!" % path)
     else:
-        return p
+        return path
 
-def is_valid_month(m):
-    m = int(m)
-    if m < 1 or m > 12:
-        raise argparse.ArgumentTypeError("Month must to be in rage 1..12")
-    return m
+def is_valid_month(month):
+    if re.match(r"\d{2}/\d{4}", month):
+        return month
+    else:
+        raise argparse.ArgumentTypeError("Invalid month format. MM/YYYY expected")
 
 parser = argparse.ArgumentParser(description="Consumption excel parser")
 parser.add_argument("-p","--path", dest="filename", required=True,
-                    help="path to excel file", metavar="FILE", type=is_valid_file)
+            help="path to excel file", metavar="FILE", type=is_valid_file)
 parser.add_argument("-m", "--month", dest="month", required=True,
-                    help="report's month", type=is_valid_month)
+            help="report's month in format MM/YYYY", type=is_valid_month)
 parser.add_argument("-s", "--source", dest="source", required=True,
-                    help="excel's file source (source name_[last 4 digits of credid card or bank account number])")
+            help="excel's file source (source name_[credit card or bank account number])")
 args = parser.parse_args()
 
 parserName = args.source.split('_')[0]
 parser_method = getattr(dataParser, "parse_%s" % parserName)
 entities = parser_method(args)
 for de in entities:
-    print(de)
+    print de
