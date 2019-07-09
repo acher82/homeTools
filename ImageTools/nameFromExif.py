0fd51599ba6ca6e04d31c10267b70e9dd24eda52
acher82 <chernomordik@gmail.com>
2019-07-09 20:43:58 +0300

    Refactor nameFromExif tool: - Add command line argument parser - Add new fromat parameter - Wrap loop body with try except

--- ImageTools/nameFromExif.py
+++ ImageTools/nameFromExif.py
@@ -1,28 +1,35 @@
+
+import argparse
+import os
+import random
 import sys
 from datetime import datetime
-from os import listdir, rename
-from os.path import isfile, join
 
 import exifread
 
-if (len(sys.argv)==1):
-    print("Provide a path")
-    sys.exit(1)
+parser = argparse.ArgumentParser(description="Renames image files in folder to name based on 'EXIF DateTimeOriginal' field")
+parser.add_argument("-p","--path", dest="path", required=True,
+            help="path")
+parser.add_argument("-f","--format", dest="format", default="IMG_%Y%m%d_%H%M%S.jpg",
+            help="new name format")
+args = parser.parse_args()
 
-mypath = sys.argv[1]
-print(mypath)
-
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-for file in onlyfiles:
-    f = open(join(mypath,file), 'rb')
-    tags = exifread.process_file(f)
-    f.close()
-    
+only_files = [f for f in os.listdir(args.path) if os.path.isfile(os.path.join(args.path, f))]
+for file in only_files:
     sys.stdout.write(file)
-    dateStr = tags['EXIF DateTimeOriginal']
-    dt = datetime.strptime(dateStr.__str__(), "%Y:%m:%d %H:%M:%S")
-    newName = dt.strftime("IMG_%Y%m%d_%H%M%S.jpg")
-    rename(join(mypath, file), join(mypath, newName))
     sys.stdout.write("-->")
-    sys.stdout.write(newName)
-    print
+    try:
+        f = open(os.path.join(args.path,file), 'rb')
+        tags = exifread.process_file(f)
+        f.close()
+        
+        dateStr = tags['EXIF DateTimeOriginal']
+        dt = datetime.strptime(dateStr.__str__(), "%Y:%m:%d %H:%M:%S")
+        newName = dt.strftime(args.format)
+        if os.path.exists(os.path.join(args.path, newName)):
+            newName = newName.replace(".jpg", "_{}.jpg".format(random.randint(1000,9999)))
+        os.rename(os.path.join(args.path, file), os.path.join(args.path, newName))
+        print(newName)
+
+    except Exception as ex:
+        print("{} {}".format(type(ex).__name__, ex))
