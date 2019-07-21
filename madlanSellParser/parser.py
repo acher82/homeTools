d50b181d2cb7e5b08e1a70ff552df5d6e62120dc
acher82 <chernomordik@gmail.com>
2019-07-21 20:17:28 +0300

    Add parser for historical data New -t/--type parameter current/previous

--- madlanSellParser/parser.py
+++ madlanSellParser/parser.py
@@ -14,9 +14,52 @@ def is_valid_file(path):
     else:
         raise argparse.ArgumentTypeError("The file {} does not exist!".format(path))
 
+def current(jsonContent):
+    entities = []
+    dictionary = json.loads(jsonContent)["apiCache"]
+    flatsKey = [key for key in dictionary if "https://www.madlan.co.il/homes" in key][0]
+    flatsList = dictionary[flatsKey]["jointMarkers"]
+    for flat in flatsList:
+        entity=(
+            flat["record"]["id"],
+            flat["location"]["formattedAddress"],
+            flat["record"]["features"]["rooms"],
+            flat["record"]["features"]["floor"],
+            flat["record"]["features"]["buildedArea"] if "buildedArea" in flat["record"]["features"] else "",
+            flat["record"]["price"],
+            flat["record"]["publishDate"],
+            flat["record"]["updateDate"],
+            flat["record"]["features"]["floors"],
+            flat["record"]["sellerType"],
+            flat["shareTexts"]["shareUrl"])
+        entities.append(entity)
+    entities = sorted(entities, key=lambda x: x[0])
+    return entities
+
+def previous(jsonContent):
+    entities = []
+    dictionary = json.loads(jsonContent)["apiCache"]
+    flatsKey = [key for key in dictionary if "https://www.madlan.co.il/api/public/v1.0/local2/localPage" in key][0]
+    flatsList = dictionary[flatsKey]["previousSales"]
+    for flat in flatsList:
+        entity=(
+            flat["soldDate"],
+            flat["address"],
+            flat["price"],
+            flat["rooms"],
+            flat["buildedArea"],
+            flat["floor"],
+            flat["ppm"],
+            flat["buildingYear"],
+            "TV" if "agentsInfo" in flat else "")
+        entities.append(entity)
+    return entities
+
 parser = argparse.ArgumentParser(description="HTML file")
 parser.add_argument("-p","--path", dest="filename", required=True,
             help="path to HTML file", metavar="FILE", type=is_valid_file)
+parser.add_argument("-t","--type", dest="type", required=True,
+            help="type of parsing", choices=["current","previous"])
 parser.add_argument("-o", "--output", dest="output", required=False,
             help="output format", choices=["csv"])
 args = parser.parse_args()
@@ -30,25 +73,7 @@ with open(args.filename, 'rb') as file:
         if len(dev.contents) > 0 :
             if dev.contents[0].strip().startswith("window.__MADLAN_SSR_CONFIG__="):
                 jsonContent = dev.contents[0].strip().replace("window.__MADLAN_SSR_CONFIG__=","")
-                dictionary = json.loads(jsonContent)["apiCache"]
-                flatsKey = [key for key in dictionary if "https://www.madlan.co.il/homes" in key][0]
-                flatsList = dictionary[flatsKey]["jointMarkers"]
-                for flat in flatsList:
-                    entity=(
-                        flat["record"]["id"],                        
-                        flat["location"]["formattedAddress"],
-                        flat["record"]["features"]["rooms"],
-                        flat["record"]["features"]["floor"],
-                        flat["record"]["features"]["buildedArea"] if "buildedArea" in flat["record"]["features"] else "",
-                        flat["record"]["price"],
-                        flat["record"]["publishDate"],
-                        flat["record"]["updateDate"],
-                        flat["record"]["features"]["floors"],
-                        flat["record"]["sellerType"],
-                        flat["shareTexts"]["shareUrl"])
-                    entities.append(entity)
-
-entities = sorted(entities, key=lambda x: x[0])
+                entities = locals()[args.type](jsonContent)
 
 if (args.output == "csv"):
     file_name = "{}.csv".format(os.path.splitext(os.path.basename(args.filename))[0])
