c88ecfd1ebfa4e2b204c6f87c56d123cc1c807b3
acher82 <chernomordik@gmail.com>
2019-08-05 21:51:41 +0300

    Rename leumi to max Adjust report format changes

--- budgetTools/parsers/max.py
+++ budgetTools/parsers/max.py
@@ -3,6 +3,10 @@ import re
 
 from xlrd import open_workbook
 
+def convertDate(date):
+    parts = date.split("-")
+    return "{}/{}/{}".format(parts[2],parts[1],parts[0])
+
 def parse(path, month, source):    
     # tuple format date,month,sum,source,target,place
     entities = []
@@ -12,7 +16,7 @@ def parse(path, month, source):
         for row in range(sheet.nrows):
             if sheet.cell(row,0).value :
                 value = sheet.cell(row,0).value
-                if re.match(r"\d{1,2}\/\d{1,2}\/\d{4}", value):
-                    entities.append((value, month, sheet.cell(row,6).value, source, '', sheet.cell(row,2).value))
+                if re.match(r"\d{4}-\d{1,2}-\d{1,2}", value):
+                    entities.append((convertDate(value), month, sheet.cell(row,5).value, source, '', sheet.cell(row,1).value))
 
     return entities
