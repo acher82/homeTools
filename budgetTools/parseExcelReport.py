f94c9989328f5529ee8fc017cd1b55f3d81ede73
acher82 <chernomordik@gmail.com>
2019-08-27 21:52:35 +0300

    Change month format to YYYY/MM

--- budgetTools/parseExcelReport.py
+++ budgetTools/parseExcelReport.py
@@ -13,16 +13,16 @@ def is_valid_file(path):
         raise argparse.ArgumentTypeError("The file {} does not exist!".format(path))
 
 def is_valid_month(month):
-    if re.match(r"^(0?[1-9]|1[012])\/(19|20)\d{2}$", month):
+    if re.match(r"^(19|20)\d{2}\/(0?[1-9]|1[012])$", month):
         return month
     else:
-        raise argparse.ArgumentTypeError("Invalid month format. MM/YYYY expected")
+        raise argparse.ArgumentTypeError("Invalid month format. YYYY/MM expected")
 
 parser = argparse.ArgumentParser(description="Consumption excel parser")
 parser.add_argument("-p","--path", dest="filename", required=True,
             help="path to excel file", metavar="FILE", type=is_valid_file)
 parser.add_argument("-m", "--month", dest="month", required=True,
-            help="report's month in format MM/YYYY", type=is_valid_month)
+            help="report's month in format YYYY/MM", type=is_valid_month)
 parser.add_argument("-st", "--source-type", dest="source_type", required=True,
             help="excel's file source type")
 parser.add_argument("-sn", "--source-name", dest="source_name", required=False,
