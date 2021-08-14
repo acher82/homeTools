6e5b066c05e52328ff894171ce6d37348b4d616f
acher82 <chernomordik@gmail.com>
2021-08-14 18:40:24 +0300

    Major refactor: - Break code to functions - Define parsable row - More strict selectors

--- yad2FlatSellParser/parser.py
+++ yad2FlatSellParser/parser.py
@@ -1,49 +1,77 @@
 # -*- coding: utf-8 -*-
 import argparse
 import csv
-import os.path
+import collections
+import glob
+import os
+import re
 
 from bs4 import BeautifulSoup
 
-
-def is_valid_file(path):
-    if os.path.exists(path):
-        return path
-    else:
+def is_valid_path(path):
+    if not os.path.exists(path):
         raise argparse.ArgumentTypeError("The file {} does not exist!".format(path))
+    return path
 
-parser = argparse.ArgumentParser(description="HTML file")
-parser.add_argument("-p","--path", dest="filename", required=True,
-            help="path to HTML file", metavar="FILE", type=is_valid_file)
-parser.add_argument("-o", "--output", dest="output", required=False,
+def parsable_row(tag):
+    return not (
+        tag.has_attr('is-platinum') or
+        'boost_realtor' in tag['class']
+    )
+
+def parse_row(tag):
+    response = {}
+    response["id"] = tag["item-id"]
+    response["address"] = tag.find("span", {"class" : "title"}).contents[0].strip()
+    response["rooms"] = tag.find("span",
+        {"id" : re.compile('^data_rooms_\\d+$')}).contents[0].strip()
+    response["floor"] = tag.find("span",
+        {"id" : re.compile('^data_floor_\\d+$')}).contents[0].strip()
+    response["sqrm"] = tag.find("span",
+        {"id" : re.compile('^data_SquareMeter_\\d+$')}).contents[0].strip()
+    response["price"] = ''.join(c for c in tag.find("div",
+        {"class" : "price"}).contents[0].strip() if c.isdigit())
+    response["updated"] = tag.find("div", {"class" : "showDateInLobby"}).contents[0].strip()
+    merchant = tag.find("div", {"class" : "merchant_name"})
+    if merchant is not None:
+        response["merchant"] = merchant.contents[1].strip()
+    else:
+        response["merchant"] = ""
+
+    return collections.namedtuple("FlatData", response.keys())(*response.values())
+
+def parse_page(file_name):
+    response = []
+    with open(file_name, 'rb') as html_file:
+        content = html_file.read()
+        soup = BeautifulSoup(content, 'html.parser')
+
+    for dev in soup.find_all("div", {"id" : re.compile('^feed_item_\\d+$')}):
+        if parsable_row(dev):
+            response.append(
+                parse_row(dev)
+            )
+
+    return response
+
+parser = argparse.ArgumentParser(description="real estate html parser")
+parser.add_argument("-p","--path", required=True,
+            help="path to a folder", type=is_valid_path)
+parser.add_argument("-f", "--filter", required=True,
+            help="files selector", default="*.html")
+parser.add_argument("-o", "--output", required=False,
             help="output format", choices=["csv"])
 args = parser.parse_args()
 
 entities = []
-with open(args.filename, 'rb') as file:
-    content = file.read()
-    soup = BeautifulSoup(content, 'html.parser')
+for file in glob.glob(f"{args.path}{args.filter}"):
+    entities = entities + parse_page(file)
 
-    for dev in soup.find_all("div", "feeditem table"):
-        item_id = dev.find("div", {"item-id" : True})["item-id"]
-        address = dev.find("span", {"class" : "title"}).contents[0].strip()
-        values = dev.find_all("span", {"class" : "val"})
-        rooms = values[0].contents[0].strip()
-        floor = values[1].contents[0].strip()
-        sqrm = values[2].contents[0].strip()
-        price = ''.join(c for c in dev.find("div", {"class" : "price"}).contents[0].strip() if c.isdigit())
-        date = dev.find("div", {"class" : "merchant_name"})
-        if date is not None:
-            date = date.contents[1].strip()
-        else:
-            date = dev.find("span", {"class" : "date"}).contents[0].strip()
-        entities.append((item_id, address, rooms, floor, sqrm, price, date))
+entities.sort(key=lambda x: x.id)
 
-entities = sorted(list(set(entities)), key=lambda x: x[0])
-
-if (args.output == "csv"):
-    file_name = "{}.csv".format(os.path.splitext(os.path.basename(args.filename))[0])
-    with open(file_name, 'wt') as output_file:
+if args.output == "csv":
+    out_file_name = "output.csv"
+    with open(out_file_name, 'wt') as output_file:
         file_writer = csv.writer(output_file)
         file_writer.writerows(entities)
 else:
